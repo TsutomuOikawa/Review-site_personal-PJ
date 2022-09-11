bc87240f93dd35d4d7e487c65f0cc1f2e23c9b06
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-09-11 19:00:10 +0900

    右サイドバー切り離し
    マイページメニューである右サイドバーを切り離しました。今後、マイページメニューにあたる各ページのサイドバーの記述も修正します。

---
+++ sidebarRight.php
@@ -0,0 +1,13 @@
+<!--　右サイドバー　-->
+
+  <aside id="right-sidebar">
+    <div class="sidebar-inner align-center">
+      <div class="sidebar-title"><p>マイページメニュー</p></div>
+      <ul>
+        <li><a href="mypage.php">マイページトップ</a></li>
+        <li><a href="profileEdit.php">プロフィール編集</a></li>
+        <li><a href="passChange.php">パスワード変更</a></li>
+        <li><a href="withdrawal.php">退会</a></li>
+      </ul>
+    </div>
+  </aside>
