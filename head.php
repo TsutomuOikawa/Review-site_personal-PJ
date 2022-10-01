31bb84dbfaab6ce454a4084b1ab24aa33ec424cf
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-10-01 13:54:54 +0900

    フォントアイコン
    CDN用URLを追加

--- head.php
+++ head.php
@@ -6,6 +6,7 @@
   <link rel="stylesheet" href="reset.css">
   <link rel="stylesheet" href="common.css">
   <link rel="stylesheet" href=<?php echo $css_title .'.css';?> >
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
   <title><?php echo $p_title;?>｜Concent-rate</title>
 </head>
