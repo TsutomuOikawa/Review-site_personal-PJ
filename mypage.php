d3d96538b216ca7263f410c08099c193cf2b5508
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-10-15 23:09:23 +0900

    マイページCSS設計
    mypage.cssの内容も統合

--- mypage.php
+++ mypage.php
@@ -12,11 +12,9 @@ $u_id = $_SESSION['user_id'];
 $dbMypageData = getMypageData($u_id);
 // debug('$dbMypagedataの値：'.print_r($dbMypageData,true));
 
-
  ?>
 
 <?php
-$css_title = basename(__FILE__,".php");
 $p_title = 'マイページ';
 //共通headタグ呼び出し
 require('head.php');
@@ -29,104 +27,113 @@ require('header.php');
 <div id="js_show_msg" style="display:none;" class="js_msg_window" >
   <p><?php echo getSessionMsg('js-msg'); ?></p>
 </div>
-<div class="wrap">
-  <main>
-    <div class="h1-wide">
-      <h1>マイページトップ</h1>
-    </div>
-    <div class="mypage-inner">
 
-      <section>
-        <div class="h2_space">
-          <h2>お気に入りの施設</h2>
-        </div>
-        <div class="myInstList scrollView">
-          <?php foreach ($dbMypageData['favorite'] as $key => $val):?>
-          <div class="myInstList__item">
-            <a href="searchDetail.php?i=<?php echo $val['id']; ?>">
-              <img src="<?php echo $val['image']; ?>" class="myInstList__item__img panelItem" alt="">
-              <p class="panelItem"><?php echo $val['name']; ?></p>
-              <p class="font-sizeS"><?php echo $val['prefecture'].$val['city']; ?></p>
-              <p class="panelItem font-sizeS"><?php echo $val['type']; ?></p>
-              <div class="pointItem">
-                <span class="material-icons md-18">grade</span>
-              </div>
-              <p class="font-sizeS totalPt totalPt--s"><?php echo isEmpty((int)$val['t_avg'], 2); ?></p>
-            </a>
-          </div>
-        <?php endforeach; ?>
-        </div>
-      </section>
+<div class="pageWrapper">
+  <h1 class="pageTitle">マイページトップ</h1>
+  <div class="pageContents pageContents--between">
 
-      <section>
-        <div class="h2_space">
-          <h2>投稿済みのクチコミ</h2>
-        </div>
-        <?php if(empty($dbMypageData['review'])): ?>
-        <p class="align-center">まだクチコミ投稿がありません</p>
+    <main class="mainContents">
+      <section class="contentsWrapper contents-baseColor">
+        <h2 class="subTitle subTitle--Visual">お気に入りの施設</h2>
+        <?php if(empty($dbMypageData['favorite'])): ?>
+        <p class="noItem">お気に入り登録済みの施設がありません</p>
 
         <?php else: ?>
-        <div class="scrollView">
-          <ul>
-            <?php foreach ($dbMypageData['review'] as $key => $val):?>
-            <li class="listCard listCard--marginS">
-                <p class="panelItem font-sizeS">
-                  <span class="days"><?php echo(date('Y年m月d日',strtotime($val['create_date']))); ?>投稿｜</span><?php echo $val['name']; ?>
-                </p>
-                <div class="panelItem pointArea">
-                  <div class="starBox pointItem">
-                    <span class="material-icons md-24 <?php echo(($val['total_pt']>=1)?'active':'nonactive'); ?>">grade</span><span class="material-icons md-24 <?php echo(($val['total_pt']>=2)?'active':'nonactive') ?>">grade</span><span class="material-icons md-24 <?php echo(($val['total_pt']>=3)?'active':'nonactive') ?>">grade</span><span class="material-icons md-24 <?php echo(($val['total_pt']>=4)?'active':'nonactive') ?>">grade</span><span class="material-icons md-24 <?php echo(($val['total_pt']>=5)?'active':'nonactive') ?>">grade</span>
-                  </div>
-                  <div class="pointItem">
-                    <span class="totalPt"><?php echo $val['total_pt'] ?>.0</span>
-                  </div>
-                  <span class="pointItem">［コンセント<span class="material-icons md-18">grade</span><?php echo $val['concent_pt'];?>｜Wi-Fi<span class="material-icons md-18">grade</span><?php echo $val['wifi_pt']; ?>｜静かさ<span class="material-icons md-18">grade</span><?php echo $val['silence_pt']; ?>］</span>
+        <div class="mainContents_inner scrollView">
+
+          <?php foreach ($dbMypageData['favorite'] as $key => $val):?>
+          <a href="searchDetail.php?i=<?php echo $val['id']; ?>" class="panelLink panelLink--float --hoverFlow">
+            <img src="<?php echo $val['image']['path']; ?>" class="panelLink_img" alt="">
+            <div class="panelLink_description">
+              <span class= "typeTag --tagS"><?php echo $val['type']; ?></span>
+              <p class="smallTitle"><?php echo $val['name']; ?></p>
+              <div class="pointArea">
+                <div class="pointArea_starBox">
+                  <span class="material-icons md-18">grade</span>
                 </div>
-                <p class="panelItem borderSeparate"><?php echo $val['purpose']; ?>で利用｜滞在時間：<?php echo $val['stay'];?></p>
+                <p class="pointArea_totalPt --pointS"><?php echo isEmpty((int)$val['t_avg'], 2); ?></p>
+              </div>
+              <p class="font-sizeS"><?php echo $val['prefecture'].$val['city']; ?></p>
+            </div>
+          </a>
+          <?php endforeach; ?>
 
-                <h3 class="panelItem"><?php echo $val['title'];?></h3>
-                <p class="panelItem"><?php echo $val['comment'];?></p>
-                <ul class="panelItem display-flex">
-                  <?php if (!empty($val['image'])): ?>
-                  <?php foreach ($val['image'] as $id => $pic):?>
-                  <li class="imgList"><img src="<?php echo $pic['path']; ?>" class="imgList__img" alt="<?php echo '画像'.$id.':'.$val['name']; ?>"></li>
-                  <?php endforeach; ?>
-                  <?php endif; ?>
-                </ul>
-
-            </li>
-            <?php endforeach; ?>
-          </ul>
         </div>
         <?php endif; ?>
       </section>
 
-      <section>
-        <div class="h2_space">
-          <h2>あなたが新規追加した施設</h2>
-        </div>
-        <div class="myInstList scrollView">
-          <?php foreach ($dbMypageData['registration'] as $key => $val):?>
-          <div class="myInstList__item">
-            <a href="searchDetail.php?i=<?php echo $val['id']; ?>">
-              <img src="<?php echo $val['image']; ?>" class="myInstList__item__img panelItem" alt="">
-              <p class="panelItem"><?php echo $val['name']; ?></p>
-              <p class="font-sizeS"><?php echo $val['prefecture'].$val['city']; ?></p>
-              <p class="panelItem font-sizeS"><?php echo $val['type']; ?></p>
-              <div class="pointItem">
-                <span class="material-icons md-18">grade</span>
+      <section class="contentsWrapper contents-baseColor">
+        <h2 class="subTitle subTitle--Visual">投稿済みのクチコミ</h2>
+        <?php if(empty($dbMypageData['review'])): ?>
+        <p class="noItem">まだクチコミ投稿がありません</p>
+
+        <?php else: ?>
+        <ul class="mainContents_inner scrollView">
+
+          <?php foreach ($dbMypageData['review'] as $key => $val):?>
+          <li class="reviewCard reviewCard--visualW">
+            <div class="reviewCard_description">
+              <p class="font-sizeS"><?php echo date('Y年m月d日',strtotime($val['create_date'])).'投稿｜'.$val['name']; ?></p>
+              <div class="pointArea">
+                <div class="pointArea_starBox">
+                  <span class="material-icons md-24 <?php echo(($val['total_pt']>=1)?'active':'nonactive'); ?>">grade</span><span class="material-icons md-24 <?php echo(($val['total_pt']>=2)?'active':'nonactive') ?>">grade</span><span class="material-icons md-24 <?php echo(($val['total_pt']>=3)?'active':'nonactive') ?>">grade</span><span class="material-icons md-24 <?php echo(($val['total_pt']>=4)?'active':'nonactive') ?>">grade</span><span class="material-icons md-24 <?php echo(($val['total_pt']>=5)?'active':'nonactive') ?>">grade</span>
+                </div>
+                <p class="pointArea_totalPt"><?php echo $val['total_pt'] ?>.0</p>
+                <p class="pointArea_detailPt">［コンセント<span class="material-icons md-18 md-18--padding">grade</span><?php echo $val['concent_pt'];?>｜Wi-Fi<span class="material-icons md-18 md-18--padding">grade</span><?php echo $val['wifi_pt']; ?>｜静かさ<span class="material-icons md-18 md-18--padding">grade</span><?php echo $val['silence_pt']; ?>］</p>
               </div>
-              <p class="font-sizeS totalPt totalPt--s"><?php echo isEmpty((int)$val['t_avg'], 2); ?></p>
-            </a>
-          </div>
-        <?php endforeach; ?>
-        </div>
+              <p class="borderSeparate"><?php echo $val['purpose'].'で利用｜滞在時間：'.$val['stay'];?></p>
+              <h3 class="smallTitle"><?php echo $val['title'];?></h3>
+              <p><?php echo $val['comment'];?></p>
+            </div>
+            <div class="imgBox">
+              <?php if (!empty($val['image'])): ?>
+              <?php foreach ($val['image'] as $id => $pic):?>
+              <img src="<?php echo $pic['path']; ?>" class="imgBox_img --imgM --3img" alt="<?php echo '画像'.$id.':'.$val['name']; ?>">
+              <?php endforeach; ?>
+              <?php endif; ?>
+            </div>
+          </li>
+          <?php endforeach; ?>
+
+        </ul>
+        <?php endif; ?>
       </section>
 
-    </div>
-  </main>
+      <section class="contentsWrapper contents-baseColor">
+        <h2 class="subTitle subTitle--Visual">あなたが新しく追加した施設</h2>
+        <?php if(empty($dbMypageData['favorite'])): ?>
+        <p class="noItem">追加した施設はありません</p>
 
-  <?php require('sidebarRight.php'); ?>
+        <?php else: ?>
+        <div class="mainContents_inner scrollView">
+
+          <?php foreach ($dbMypageData['registration'] as $key => $val):?>
+          <div class="panelLink panelLink--float">
+            <img src="<?php echo $val['image']; ?>" class="panelLink_img" alt="">
+            <div class="panelLink_description">
+              <span class="typeTag --tagS"><?php echo $val['type']; ?></span>
+              <p class="smallTitle"><?php echo $val['name']; ?></p>
+              <div class="pointArea">
+                <div class="pointArea_starBox">
+                  <span class="material-icons md-18">grade</span>
+                </div>
+                <p class="pointArea_totalPt --pointS"><?php echo isEmpty((int)$val['t_avg'], 2); ?></p>
+              </div>
+              <div>
+                <a href="searchDetail.php?i=<?php echo $val['id']; ?>" class="connectedLink connectedLink--left link--full --hoverFlow">詳細をみる</a>
+                <a href="institutionRegi.php?i=<?php echo $val['id']; ?>" class="connectedLink connectedLink--right link--full --hoverFlow">編集する</a>
+              </div>
+            </div>
+          </div>
+          <?php endforeach; ?>
+
+        </div>
+        <?php endif; ?>
+      </section>
+    </main>
+
+    <?php require('sidebarRight.php'); ?>
+  </div>
 </div>
 
 <!--　共通フッター呼び出し　-->
