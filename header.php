d1484d981d2b33269b5d50ef77735facab6636f8
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-10-15 22:58:05 +0900

    ヘッダークラス変更
    CSS設計に合わせてクラス名を変更

--- header.php
+++ header.php
@@ -17,8 +17,8 @@ if (!empty($_SESSION)) {
   <div class="header_wrapper header_wrapper--display">
     <a href="index.php" class="headerTitle"><!--<img src="#" class="logoImg" alt="サービスロゴ">-->Concent-rate</a>
     <ul class="headerNav">
-      <li class="headerNav_Item"><a href=<?php echo $header[0]; ?> class="header-link link--hover"><?php echo $header[1]; ?></a></li>
-      <li class="headerNav_Item headerNav_Item--strong"><a href=<?php echo $header[2]; ?> class="header-link link--hover"><?php echo $header[3]; ?></a></li>
+      <li class="headerNav_Item"><a href=<?php echo $header[0]; ?> class="headerLink --hoverLine"><?php echo $header[1]; ?></a></li>
+      <li class="headerNav_Item headerNav_Item--strong"><a href=<?php echo $header[2]; ?> class="headerLink --hoverFlow"><?php echo $header[3]; ?></a></li>
     </ul>
   </div>
 </header>
