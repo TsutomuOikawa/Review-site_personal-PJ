49d0cd38553dc06bff121bcabaeb9a970fe431ef
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-10-15 22:55:05 +0900

    ログアウト時のjsメッセージ追加

--- logout.php
+++ logout.php
@@ -7,9 +7,11 @@ $debug_current_page = basename(__FILE__);
 debugLogStart();
 //=========================================
 //ログアウト機能
-//セッションを破棄
-session_destroy();
-debug('セッション破棄完了・ログインページへ遷移します');
+//セッション情報を初期化
+$_SESSION = array();
+
+debug('セッション情報破棄完了・ログインページへ遷移します');
+$_SESSION['js-msg'] = JSMSG06;
 
 //ログインページへ移動
 header("Location:login.php");
