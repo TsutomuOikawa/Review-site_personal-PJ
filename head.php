80cf0aa189fb35e741bb0e3b26e2f7608dea800d
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-11-03 13:32:41 +0900

    CSSファイルのディレクトリ変更
    cssシート統合に伴い、common.cssをstyle.cssに変更

--- head.php
+++ head.php
@@ -3,8 +3,9 @@
 <html lang="ja" dir="ltr">
 <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" href="reset.css">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="stylesheet" href="common.css">
+  <link rel="stylesheet" href="icon/css/all.css">
   <title><?php echo $p_title;?>｜Concent-rate</title>
 </head>
