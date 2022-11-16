d7301b862a7824af274c78d07adf1c37614bf294
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-11-16 15:47:01 +0900

    add:フッターメニュー、レスポンシブ対応
    フッターにメニューがなかったため、追加。レスポンシブ対応も追加

--- footer.php
+++ footer.php
@@ -1,7 +1,16 @@
 <footer id="footer" class="footer">
-  <small class="footer_copyright"> Copyright @ GlobeNote <br>All Rights Reserved</small>
+  <div class="footer-wrapper">
+    <ul class="footer_nav">
+      <li class="footer_navItem"><a href="searchList.php" class="--hoverLine">施設検索</a></li>
+      <li class="footer_navItem"><a href="institutionRegi.php" class="--hoverLine">施設新規追加</a></li>
+      <li class="footer_navItem"><a href="#" class="--hoverLine">プライバシーポリシー</a></li>
+      <li class="footer_navItem"><a href="#" class="--hoverLine">利用規約</a></li>
+      <li class="footer_navItem"><a href="#" class="--hoverLine">お問い合わせ</a></li>
+    </ul>
+    <small class="footer_copyright"> Copyright @ <a href="index.php" class="--hoverLine">Concent-rate</a> <br>All Rights Reserved</small>
+  </div>
 </footer>
-<script src="js/jquery-3.6.0.min.js" charset="utf-8"></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 <script src="js/app.js" charset="utf-8"></script>
 </body>
 </html>
