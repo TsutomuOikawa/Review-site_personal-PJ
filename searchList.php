ced6f64218a5d9afe590c90073e5cc80a6bfa177
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-11-18 15:32:05 +0900

    update:レスポンシブル対応

--- searchList.php
+++ searchList.php
@@ -60,82 +60,87 @@ require('header.php');
 
 <!--　メインコンテンツ　-->
 <div class="page-wrapper">
-  <h1 class="page_title">施設情報一覧</h1>
 
-  <div class="page_contents--between">
+  <div class="container">
+    <h1 class="container_title">施設情報一覧</h1>
+    <div class="container_body container_body--divide">
 
-    <?php require('sidebarLeft.php'); ?>
-    <!--　検索結果一覧　-->
-    <main class="mainContents-wrapper">
-      <div class="display-between">
-        <span class="subTitle"><?php echo '検索結果：'.$dbInstList['total_data'].'件'; ?></span>
-        <span style="margin:auto 35px;"><?php echo ($currentMinNum +1);?>-<?php echo (($currentMinNum + $listSpan)<$dbInstList['total_data'])?($currentMinNum + $listSpan):$dbInstList['total_data']; ?>件／<?php echo $dbInstList['total_data'] ?>件中</span>
-      </div>
+      <?php require('sidebarLeft.php'); ?>
+      <main class="container_mainBody--list module instList">
 
-      <?php if ($dbInstList['total_data'] === 0): ?>
-        <p class="noItem">検索条件に該当する施設がありません<br>
-        条件を変更して再度お試しください</p>
-
-      <?php else: ?>
-        <ul class="scrollView-wrapper baseColor">
-          <?php foreach ($dbInstListReview as $key => $value): ?>
-          <li class="reviewCard reviewCard--sizeM">
-            <?php if(!empty($value['inst']['purpose'])) echo '<a href="#" class="tag --tagM tag--purpose">'.$value['inst']['purpose'].'におすすめ</a>'; ?>
-            <div class="display-between">
-              <h2 class="subTitle font-sizeM"><a href="searchDetail.php?i=<?php echo $value['inst']['id'].'&p='.$currentPageNum; ?>" class="--hoverLine"><?php echo $value['inst']['name']; ?></a></h2>
-              <span class="material-icons md-36 js-favorite <?php echo((isLike($_SESSION['user_id'], $value['inst']['id']))?'active':'nonactive');?>" data-instid="<?php echo $value['inst']['id']; ?>">favorite</span>
-            </div>
-
-            <div class="display-between">
-              <div class="reviewCard_imgArea">
-                <img src="<?php echo $value['image'][0]['path']; ?>" class="--imgL" alt="">
-                <div class="imgBox">
-                  <?php for ($i=1; $i <= 3 ; $i++):?>
-                  <img src="<?php echo $value['image'][$i]['path']; ?>" alt="" class="--imgS --3img">
-                  <?php endfor; ?>
-                </div>
-              </div>
-
-              <div class="reviewCard_description">
-                <p><a href="#" class="link--hull tag tag--type --tagM"><?php echo $value['inst']['type']; ?></a></p>
-                <div class="pointArea">
-                  <div class="pointArea_starBox">
-                    <span class="material-icons md-24 <?php echo(($value['inst']['t_avg']>=1)?'active':'nonactive'); ?>">grade</span><span class="material-icons md-24 <?php echo(($value['inst']['t_avg']>=2)?'active':'nonactive') ?>">grade</span><span class="material-icons md-24 <?php echo(($value['inst']['t_avg']>=3)?'active':'nonactive') ?>">grade</span><span class="material-icons md-24 <?php echo(($value['inst']['t_avg']>=4)?'active':'nonactive') ?>">grade</span><span class="material-icons md-24 <?php echo(($value['inst']['t_avg']>=5)?'active':'nonactive') ?>">grade</span>
-                  </div>
-                  <p class="pointArea_totalPt"><?php echo (($value['inst']['t_avg'])? number_format($value['inst']['t_avg'], 2) : '-.--'); ?></p>
-                  <p class="font-sizeS"><a href="searchDetail.php?i=<?php echo $value['inst']['id'].'&p='.$currentPageNum;?>#reviews" class="--hoverLine"><?php echo (($value['inst']['total_review'])? $value['inst']['total_review'] : 0); ?>件のクチコミ</a></p>
-                </div>
-
-                <ul class="display-flex borderSeparate">
-                  <?php if($value['inst']['concent']==='1') echo'<li class="tag tag--feature"><a href="#" class="link--full --tagM">コンセントあり</a></li>'; ?>
-                  <?php if($value['inst']['wifi']==='1') echo'<li class="tag tag--feature"><a href="#" class="link--full --tagM">Wi-fiあり</a></li>'; ?>
-                  <?php if($value['inst']['stay_id'] >= 5) echo'<li class="tag tag--feature"><a href="#" class="link--full --tagM">'.$value['inst']['stay'].'滞在</a></li>'; ?>
-                  <?php if($value['inst']['s_avg'] >= 3.5) echo '<li class="tag tag--feature"><a href="#" class="link--full --tagM">集中しやすい環境</a></li>';?>
-                </ul>
-                <div class="font-sizeS borderSeparate">
-                  <p>営業時間：<?php echo $value['inst']['hours']; ?></p>
-                  <p>定休日：<?php echo $value['inst']['holidays']; ?></p>
-                  <p>アクセス：<?php echo ($value['inst']['access'])? $value['inst']['access'] :'ー'; ?></p>
-                </div>
-
-                <div class="font-sizeS">
-                  <?php for ($i=0; $i < 2; $i++){
-                    $date = (!empty($value['latest_review'][$i]))? date('Y/m/d',strtotime($value['latest_review'][$i]['create_date'])).'：' : '';
-                    $comment = (!empty($value['latest_review'][$i])) ? $value['latest_review'][$i]['title'] : 'ーー';
-                    echo '<p>'.$date.$comment.'</p>';
-                  };?>
-                </div>
-              </div>
-            </div>
-          </li>
-          <?php endforeach; ?>
-        </ul>
-
-        <div>
-          <?php require('pagination.php'); ?>
+        <div class="instList_number">
+          <p class="module_title"><?php echo '検索結果：'.$dbInstList['total_data'].'件'; ?></p>
+          <p><?php echo ($currentMinNum +1);?>-<?php echo (($currentMinNum + $listSpan)<$dbInstList['total_data'])?($currentMinNum + $listSpan):$dbInstList['total_data']; ?>件／<?php echo $dbInstList['total_data'] ?>件中</p>
         </div>
-      <?php endif; ?>
-    </main>
+        <div class="instList_body">
+          <?php if ($dbInstList['total_data'] === 0): ?>
+            <p class="noItem">検索条件に該当する施設がありません<br>
+            条件を変更して再度お試しください</p>
+
+          <?php else: ?>
+            <ul class="reviewCard-wrapper">
+              <?php foreach ($dbInstListReview as $key => $value): ?>
+              <li class="reviewCard reviewCard--sizeM">
+                <?php if(!empty($value['inst']['purpose'])) echo '<a href="#" class="tag --tagM tag--purpose tag--leftTop">'.$value['inst']['purpose'].'におすすめ</a>'; ?>
+                <i class="fa-solid fa-heart fa-2x reviewCard_favorite js-favorite-animation <?php echo((isLike($_SESSION['user_id'], $value['inst']['id']))?'active':'nonactive');?>" data-instid="<?php echo $value['inst']['id'];?>"></i>
+                <i class="fa-solid fa-heart fa-2x reviewCard_favorite js-favorite <?php echo((isLike($_SESSION['user_id'], $value['inst']['id']))?'active':'nonactive');?>" data-instid="<?php echo $value['inst']['id'];?>"></i>
+                <h2 class="reviewCard_name"><a href="searchDetail.php?i=<?php echo $value['inst']['id'].'&p='.$currentPageNum; ?>" class="--hoverLine"><?php echo $value['inst']['name']; ?></a></h2>
+
+                <div class="reviewCard_body">
+                  <div class="reviewCard_imgArea">
+                    <img src="<?php echo $value['image'][0]['path']; ?>" class="--imgL" alt="">
+                    <div class="imgBox">
+                      <?php for ($i=1; $i <= 3 ; $i++):?>
+                      <img src="<?php echo $value['image'][$i]['path']; ?>" alt="" class="--imgS --3img">
+                      <?php endfor; ?>
+                    </div>
+                  </div>
+
+                  <div class="reviewCard_description">
+                    <p><a href="#" class="tag tag--type --tagM"><?php echo $value['inst']['type']; ?></a></p>
+                    <div class="pointArea">
+                      <div class="pointArea_total">
+                        <div class="pointArea_starBox">
+                      <?php for ($i=1; $i <= 5; $i++):?>
+                          <i class="fa-sharp fa-solid fa-star fa-lg <?php if($value['inst']['t_avg']<$i) echo 'nonactive';?>"></i>
+                      <?php endfor; ?>
+                        </div>
+                        <p class="pointArea_totalPt"><?php echo (($value['inst']['t_avg'])? number_format($value['inst']['t_avg'], 2) : '-.--'); ?></p>
+                      </div>
+                      <p class="font-sizeS"><a href="searchDetail.php?i=<?php echo $value['inst']['id'].'&p='.$currentPageNum;?>#reviews" class="--hoverLine"><?php echo (($value['inst']['total_review'])? $value['inst']['total_review'] : 0); ?>件のクチコミ</a></p>
+                    </div>
+
+                    <ul class="reviewCard_featureTag borderSeparate">
+                      <?php if($value['inst']['concent']==='1') echo'<li class="tag tag--feature"><a href="#" class="link--full --tagM">コンセントあり</a></li>'; ?>
+                      <?php if($value['inst']['wifi']==='1') echo'<li class="tag tag--feature"><a href="#" class="link--full --tagM">Wi-fiあり</a></li>'; ?>
+                      <?php if($value['inst']['stay_id'] >= 5) echo'<li class="tag tag--feature"><a href="#" class="link--full --tagM">'.$value['inst']['stay'].'滞在</a></li>'; ?>
+                      <?php if($value['inst']['s_avg'] >= 3.5) echo '<li class="tag tag--feature"><a href="#" class="link--full --tagM">集中しやすい環境</a></li>';?>
+                    </ul>
+                    <div class="font-sizeS borderSeparate">
+                      <p>営業時間：<?php echo $value['inst']['hours']; ?></p>
+                      <p>定休日：<?php echo $value['inst']['holidays']; ?></p>
+                      <p>アクセス：<?php echo ($value['inst']['access'])? $value['inst']['access'] :'ー'; ?></p>
+                    </div>
+
+                    <div class="font-sizeS">
+                      <?php for ($i=0; $i < 2; $i++){
+                        $date = (!empty($value['latest_review'][$i]))? date('Y/m/d',strtotime($value['latest_review'][$i]['create_date'])).'：' : '';
+                        $comment = (!empty($value['latest_review'][$i])) ? $value['latest_review'][$i]['title'] : 'ーー';
+                        echo '<p>'.$date.$comment.'</p>';
+                      };?>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <?php endforeach; ?>
+            </ul>
+            <div>
+              <?php require('pagination.php'); ?>
+            </div>
+          <?php endif; ?>
+        </div>
+      </main>
+    </div>
   </div>
 </div>
 
