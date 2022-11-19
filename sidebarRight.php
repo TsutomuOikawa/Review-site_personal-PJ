bafa6a440e91fdd35da0501a67f40521de338d6e
Tsutomu Oikawa <86523154+TsutomuOikawa@users.noreply.github.com>
2022-11-19 18:02:46 +0900

    change:サイドバーメニューの変更

--- sidebarRight.php
+++ sidebarRight.php
@@ -1,12 +1,11 @@
 <!--　右サイドバー　-->
-  <aside id="sidebar" class="baseColor">
-    <p class="sidebar_title subTitle--visual">マイページメニュー</p>
+  <aside id="sidebar" class="sidebar sidebar--right">
+    <p class="sidebar_title">マイページメニュー</p>
     <nav class="sidebar_contents">
       <ul>
-        <li class="sidebar_listItem"><a href="mypage.php" class="link--full sidebar_link --hoverFlow">マイページトップ</a></li>
         <li class="sidebar_listItem"><a href="profileEdit.php" class="link--full sidebar_link --hoverFlow">プロフィール編集</a></li>
         <li class="sidebar_listItem"><a href="passChange.php" class="link--full sidebar_link --hoverFlow">パスワード変更</a></li>
-        <li class="sidebar_listItem"><a href="logout.php" class="link--full sidebar_link --hoverFlow">ログアウト</a></li>
+        <li class="sidebar_listItem"><a href="institutionRegi.php" class="link--full sidebar_link --hoverFlow">施設新規登録</a></li>
         <li class="sidebar_listItem"><a href="withdrawal.php" class="link--full sidebar_link --hoverFlow">退会</a></li>
       </ul>
     </nav>
